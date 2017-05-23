70c9cdffaf590e9cd8ef69109fc5b0ad7bb871cc
tripeno <victor.tripeno@gft.com>
2017-05-23 15:26:32 -0300

    first commit

---
+++ app/proguard-rules.pro
@@ -0,0 +1,74 @@
+# Add project specific ProGuard rules here.
+# By default, the flags in this file are appended to flags specified
+# in C:\Users\vrto\AppData\Local\Android\Sdk/tools/proguard/proguard-android.txt
+# You can edit the include path and order by changing the proguardFiles
+# directive in build.gradle.
+#
+# For more details, see
+#   http://developer.android.com/guide/developing/tools/proguard.html
+
+# Add any project specific keep options here:
+
+# If your project uses WebView with JS, uncomment the following
+# and specify the fully qualified class name to the JavaScript interface
+# class:
+#-keepclassmembers class fqcn.of.javascript.interface.for.webview {
+#   public *;
+#}
+
+# Uncomment this to preserve the line number information for
+# debugging stack traces.
+#-keepattributes SourceFile,LineNumberTable
+
+# If you keep the line number information, uncomment this to
+# hide the original source file name.
+#-renamesourcefileattribute SourceFile
+-injars      bin/classes
+-injars      libs/android-support-v13.jar
+-outjars     bin/classes-processed.jar
+-libraryjars /usr/bin/adt/sdk/platforms/android-19/android.jar
+-libraryjars libs/poi-3.10.1.jar
+
+-dontpreverify
+-repackageclasses ''
+-allowaccessmodification
+-optimizations !code/simplification/arithmetic
+-keepattributes *Annotation*
+
+-dontwarn org.apache.poi.**
+-dontwarn com.google.**
+-dontwarn android.support.**
+
+-keep public class * extends android.app.Activity
+-keep public class * extends android.support.v4.app.FragmentActivity
+-keep public class * extends android.support.v4.app.Fragment
+-keep public class * extends android.app.Application
+-keep public class * extends android.app.Service
+-keep public class * extends android.content.BroadcastReceiver
+
+-keep class com.android.vending.billing.**
+
+-keep public class * extends android.view.View {
+    public <init>(android.content.Context);
+    public <init>(android.content.Context, android.util.AttributeSet);
+    public <init>(android.content.Context, android.util.AttributeSet, int);
+    public void set*(...);
+}
+
+-keepclasseswithmembers class * {
+    public <init>(android.content.Context, android.util.AttributeSet);
+}
+
+-keepclasseswithmembers class * {
+    public <init>(android.content.Context, android.util.AttributeSet, int);
+}
+
+-keepclassmembers class * implements android.os.Parcelable {
+    static android.os.Parcelable$Creator CREATOR;
+}
+
+-keepclassmembers class **.R$* {
+    public static <fields>;
+}
+
+-keep public class org.apache.poi.** {*;}
---
+++ app/src/main/java/com/example/vrto/read_sheet/MainActivity.java
@@ -0,0 +1,113 @@
+package com.example.vrto.read_sheet;
+
+import android.content.res.AssetManager;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListView;
+
+import com.opencsv.CSVReader;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import jxl.Cell;
+import jxl.Sheet;
+import jxl.Workbook;
+import jxl.read.biff.BiffException;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.listaValores);
+
+        try {
+            //System.out.println(retornoPlanilhaJxl());
+            Log.d("Minha Lista XSL", retornoPlanilhaJxl().toString());
+            //Log.d("Minha Lista CSV", retornoPlanilhaCsv().toString());
+            List<String[]>  lista = retornoPlanilhaCsv();
+            for(int i = 0; i < lista.size(); i++) {
+                Log.d("Minha Lista CSV" + i, String.valueOf(lista.get(i)[0]));
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<String> retornoPlanilhaJxl(/*String key*/) throws IOException {
+        List<String> resultSet = new ArrayList<String>();
+
+        Log.d("CAMINHO", String.valueOf(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)) + "/Planilha_teste.xls");
+        File inputWorkbook = new File(String.valueOf(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)) + "/Planilha_teste.xls");
+        if(inputWorkbook.exists()){
+            Workbook w;
+            try {
+                w = Workbook.getWorkbook(inputWorkbook);
+                // Get the first sheet
+                Sheet sheet = w.getSheet(0);
+                // Loop over column and lines
+                for (int j = 0; j < sheet.getRows(); j++) {
+                    Cell cell = sheet.getCell(0, j);
+                   // if(cell.getContents().equalsIgnoreCase(key)){
+                        for (int i = 0; i < sheet.getColumns(); i++) {
+                            Cell cel = sheet.getCell(i, j);
+                            resultSet.add(cel.getContents());
+
+                        }
+                   // }
+                    continue;
+                }
+            } catch (BiffException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        else
+        {
+            resultSet.add("File not found..!");
+        }
+        if(resultSet.size()==0){
+            resultSet.add("Data not found..!");
+        }
+        return resultSet;
+
+    }
+
+    public List<String[]> retornoPlanilhaCsv() throws IOException {
+        List<String[]> questionList = new ArrayList<String[]>();
+        AssetManager assetManager = getAssets();
+
+        try {
+            File inputWorkbook = new File(String.valueOf(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)) + "/Planilha_teste.csv");
+            Log.d("CAMINHO", inputWorkbook.toString());
+            InputStream csvStream = new FileInputStream(inputWorkbook);// assetManager.open(String.valueOf(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)) + "/Planilha_teste.csv");
+            InputStreamReader csvStreamReader = new InputStreamReader(csvStream);
+            CSVReader csvReader = new CSVReader(csvStreamReader);
+            String[] line;
+
+            // throw away the header
+            //csvReader.readNext();
+
+            while ((line = csvReader.readNext()) != null) {
+                questionList.add(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return questionList;
+    }
+}
